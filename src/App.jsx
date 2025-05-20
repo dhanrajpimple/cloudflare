123d0f05b81da0ab9a33d6eed195e66bf3691e11
dhanrajpimple <dhanrajpimple16012001@gmail.com>
2025-05-20 18:23:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import SolutionsHero from './components/SolutionsHero'
+function App() {
+
+
+  return (
+    <>
+      <SolutionsHero/>
+    </>
+  )
+}
+
+export default App
